e0da1aef4fcd1fb47ac37055441b4e9e7cf09ad8
ssmith <ssmith@montecarlodata.com>
2023-10-04 20:03:12 -0500

    dynamically build list of reports

--- insights/Extract_MC_Insights_DBX.py
+++ insights/Extract_MC_Insights_DBX.py
@@ -9,49 +9,50 @@
 
 # COMMAND ----------
 
+# DBTITLE 1,Find/Set API Credentials
+# Monte Carlo Credentials stored in DBX Secret Key Repo called "monte-carlo-creds":
+mcd_id = dbutils.secrets.get(scope = "monte-carlo-creds", key = "mc-id")
+mcd_token = dbutils.secrets.get(scope = "monte-carlo-creds", key = "mc-token")
+
+# Other variables which you can customize:
+mcd_profile = ""
+
+# COMMAND ----------
+
+# DBTITLE 1,Build a List of Available Reports
+from pycarlo.core import Client, Query, Session
+
+client = Client(session=Session(mcd_id=mcd_id, mcd_token=mcd_token, mcd_profile=mcd_profile))
+query = Query()
+query.get_insights().__fields__('name', 'reports')
+
+response = client(query).get_insights
+
+insight_name_to_report_mapping = {}
+for insight in response:
+    name = insight.name
+
+    for report in insight.reports:
+        # Some Insights have a .html report as well, we want to filter for just the .csv reports
+        if report.name.endswith('.csv'):
+            insight_name_to_report_mapping[name] = report.name
+
+# COMMAND ----------
+
 # DBTITLE 1,Create User Input Widgets
-insight_name_to_report_mapping = {
-    "key_assets": "key_assets.csv",
-    "monitors": "monitors.csv",
-    "cleanup_suggestions": "cleanup_suggestions.csv",
-    "field_cleanup_suggestions": "field_level_cleanup_suggestions.csv",
-    "field_importance_scores": "field_importance_scores.csv",
-    "events": "events.csv",
-    "table_read_write_stats": "table_read_write_stats.csv",
-    "query_runtime_trends": "query_runtime_trends.csv",
-    "rule_and_sli_results": "rule_and_sli_results.csv",
-    "insight_monitor_recom_dt_fields": "insight_monitor_recom_dt_fields.csv",
-    "insight_monitor_recom_fh_tables": "insight_monitor_recom_fh_tables.csv",
-    "insight_monitor_issues_and_solutions": "insight_monitor_issues_and_solutions.csv",
-    "insight_inactive_table_monitors": "insight_inactive_table_monitors.csv",
-    "bi_dashboard_analytics": "bi_dashboard_analytics.csv",
-    "incident_history": "incident_history.csv",
-    "heavy_queries": "heavy_queries.csv",
-    "consumption_by_user": "consumption_by_user.csv",
-    "custom_monitor_notifications": "custom_monitor_notifications.csv",
-    "unmonitored_tables": "unmonitored_tables.csv",
-}
 dbutils.widgets.multiselect(
     'INSIGHTS TO DOWNLOAD',
     defaultValue="incident_history",
     choices=list(insight_name_to_report_mapping.keys())
 )
-dbutils.widgets.text("SCHEMA TO WRITE TO", "mc_insights")
+dbutils.widgets.text("SCHEMA TO WRITE TO", "mcd_insights")
 
 # COMMAND ----------
 
-# DBTITLE 1,Runtime Variables
-# Monte Carlo Credentials stored in DBX Secret Key Repo called "monte-carlo-creds":
-mcd_id = dbutils.secrets.get(scope = "monte-carlo-creds", key = "mc-id")
-mcd_token = dbutils.secrets.get(scope = "monte-carlo-creds", key = "mc-token")
-
-# List of reports to download and load to DBX are pulled from input widgets
+# DBTITLE 1,Runtime Variables (Pulled From Input Widgets)
 insight_names = dbutils.widgets.get("INSIGHTS TO DOWNLOAD").split(',')
 insight_report_names = [insight_name_to_report_mapping[insight] for insight in insight_names]
 table_schema = dbutils.widgets.get("SCHEMA TO WRITE TO")
-
-# Other variables which you should customize:
-mcd_profile=""
 
 # COMMAND ----------
 
@@ -77,7 +78,7 @@ for i in range(len(insight_report_names)):
     r = requests.get(report_url).content
     
     # Customize the naming scheme of the loaded tables here:
-    table_name = "mc_src_"+insight_names[i]
+    table_name = "mcd_insight_" + insight_names[i]
     filename = insight_report_names[i]
     
     #Read data into pandas to convert to csv
@@ -96,5 +97,5 @@ for i in range(len(insight_report_names)):
 
 # COMMAND ----------
 
-df=spark.sql("SHOW TABLES IN {} like 'mc*'".format(table_schema))  
+df = spark.sql("SHOW TABLES IN {} like 'mcd_insight_*'".format(table_schema))
 display(df)
