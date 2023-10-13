ba6368f1c7b8b5a3fa8f7bd9f9660b1ed5615594
Michael Folino <mvfolino68@gmail.com>
2023-10-13 19:09:55 -0400

    webex update

--- notifications/webex_lambda.py
+++ notifications/webex_lambda.py
@@ -6,25 +6,29 @@ import urllib3
 from typing import Dict
 import os
 
-SHARED_SIGNING_SECRET = str.encode(os.environ.get('SHARED_SIGNING_SECRET'))  # This should be an environment variable
+SHARED_SIGNING_SECRET = os.environ.get('SHARED_SIGNING_SECRET') # any secret used to validate MC incoming webhooks
+WEBEX_ENDPOINT = os.environ.get('WEBEX_ENDPOINT') # always 'https://webexapis.com/v1/messages'
+WEBEX_ROOM_ID = os.environ.get('WEBEX_ROOM_ID') # room id of the channel where MC will deliver messages. get this from webex
+WEBEX_TOKEN = os.environ.get('WEBEX_TOKEN') # create a new bot here to retrieve token: https://developer.webex.com/my-apps/new
 
 def lambda_handler(event: Dict, context: Dict) -> Dict:
     mcd_signature = event['headers'].get('x-mcd-signature')
-    body = json.loads(event['body']) if isinstance(event['body'], str) else event['body']
+    body = json.loads(event['body'])
+    
     if verify_signature(mcd_signature=mcd_signature, body=body):
-        print('Signature Verified!')
+        print('Success!')
         webex_webhook(body)
         return {'statusCode': 200}
-    return {'statusCode': 403}
-  
+    return {'statusCode': 403}  
+    
 def verify_signature(mcd_signature: str, body: Dict) -> bool:
-    body_as_byes = urllib.parse.urlencode(body).encode('utf8')
-    computed_signature = hmac.new(SHARED_SIGNING_SECRET, body_as_byes, hashlib.sha512).hexdigest()
+    body_as_bytes = urllib.parse.urlencode(body).encode('utf8')
+    computed_signature = hmac.new(SHARED_SIGNING_SECRET.encode(), body_as_bytes, hashlib.sha512).hexdigest()
     return hmac.compare_digest(computed_signature, mcd_signature)
 
 def webex_webhook(body):
     """Hangouts Chat incoming webhook quickstart."""
-    url = os.environ.get('webex_endpoint')
+    url = os.environ.get('WEBEX_ENDPOINT')
     type = body['type']
     http = urllib3.PoolManager()
     message_text = '''
@@ -32,9 +36,11 @@ def webex_webhook(body):
     Type: {}
     URL: {}
     '''.format(type,body['payload']['url'])
-    message_headers = {'Content-Type': 'application/json'}
+    message_headers = {'Authorization': f'Bearer {WEBEX_TOKEN}',
+    'Content-Type': 'application/json'}
     bot_message = {
-        'text': message_text
+        'text': message_text,
+        'roomId': f'{WEBEX_ROOM_ID}'
     }
     encoded_message = json.dumps(bot_message)
     response = http.request("POST",url,headers = message_headers, body = encoded_message)
