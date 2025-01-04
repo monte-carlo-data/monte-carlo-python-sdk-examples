07037c5981be23f959c967ed51ed84bb7f29d285
lehrlich-mc <lehrlich@montecarlodata.com>
2025-01-03 16:07:17 -0800

    Update delete_monitors_without_tag.py

--- monitors/delete_monitors_without_tag.py
+++ monitors/delete_monitors_without_tag.py
@@ -1,3 +1,8 @@
+#INSTRUCTIONS:
+#1. Add your api key and token
+#2. Enter the tag key and value for the monitors you'd like to keep
+#3. Run the script
+
 from pycarlo.core import Client, Query, Mutation, Session
 
 # Retrieve monitors with designated tag
@@ -10,39 +15,52 @@ def get_pref_monitors(client):
     query.get_monitors(tags=filter).__fields__('uuid','tags','entities')
     search_results = client(query).get_monitors
     monitors_to_keep = []
-    count = 0
     # Adding tagged monitors to a list
     for mon in search_results:
-        print(mon)
-        monitors_to_keep.append(mon.uuid)
-        count += 1    
-    print(f"Found {count} production monitors")
+        monitors_to_keep.append(mon.uuid)   
     return monitors_to_keep
 
 # Get all custom monitors in MC
 def get_all_monitors(client, limit):
     query = Query()
-    query.get_monitors(limit=limit).__fields__('uuid')
+    query.get_monitors(limit=limit).__fields__('uuid', 'monitor_type')
     search_results = client(query).get_monitors
     return search_results
 
 # Delete a single monitor
-def delete_monitor(uuid, client):
+def delete_monitor(client, uuid, type):
     mutation = Mutation()
-    mutation.delete_custom_rule(uuid=uuid)
-    response = client(mutation).delete_custom_rule
-    print(f"Successfully deleted monitor {response.uuid}")
+    # defining monitor types that can only be deleted with delete_monitor call
+    monitor_types = ['STATS', 'FIELD_QUALITY', 'CATEGORIES', 'JSON_SCHEMA']
+    error = False
+    if type in monitor_types:
+        mutation.delete_monitor(monitor_id=uuid)
+        response = client(mutation).delete_monitor
+        if not response.success:
+            error = True
+    else:
+        mutation.delete_custom_rule(uuid=uuid)
+        response = client(mutation).delete_custom_rule
+        if not response.uuid:
+            error = True
+    if error:
+        print(f"Deletion not Successful for: {uuid}")
+    else:
+        print(f"Successfully deleted monitor {uuid}")
+    
 
 # Compiles preferred monitors and deletes every other custom monitor
 def bulk_delete_monitors(client):
     pref_monitors = get_pref_monitors(client)
-    print(pref_monitors)
+    print(f"Found {len(pref_monitors)} tagged monitors: {pref_monitors}")
     all_monitors = get_all_monitors(client,limit=200)
     count = 0
     for mon in all_monitors:
-        if mon.uuid not in prod_monitors:
-            #delete_monitor(client, mon.uuid)
-            print(f"Deleted monitor {mon.uuid}")
+        if mon.uuid not in pref_monitors:
+            mon_id = mon.uuid
+            print(mon.uuid)
+            mon_type = mon.monitor_type
+            delete_monitor(client=client, uuid=mon_id, type=mon_type)
             count+=1
     print(f"Deleted {count} monitors")
     
@@ -54,5 +72,7 @@ if __name__ == '__main__':
     #-------------------------------------------------------
     client= Client(session=Session(mcd_id=mcdId,mcd_token=mcdToken))
     bulk_delete_monitors(client)
+    
+
 
 
